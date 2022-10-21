85a81a069b8df87b075a34931f0626acf18be966
costycali <caligiore.costanza@iisgalvanimi.edu.it>
2022-10-21 09:40:37 +0000

    consegna

---
+++ esercizio1.py
@@ -0,0 +1,60 @@
+#metto  5 alla variabile x e John alla variabile y
+x = 5
+y = "John"
+print(x)
+print(y)
+print("#############")
+#attribuisco alla variabile x il valore 4 e il nome Sally
+x = 4    
+print(x)  
+x = "Sally" 
+print(x)
+
+print("#############")
+#definisco il tipo di variabile
+x = str(3)    
+x = '3'
+y = int(3)   
+z = float(3)
+z = 3.0
+print(x)
+print(y)
+print(z)
+
+
+x = 3
+y = str(x)
+
+print("#############")
+# assegno alla variabile x il valore intero 5 e alla variabile y John che è una stringa, stampo il tipo di variabile con il loro valore
+x = 5
+y = "John"
+print(int(x))
+print(str(y))
+
+print("#############")
+x = "John"
+x = 'John'
+
+print("#############")
+#nomino la prima variabile "a " di tipo intero e la seconda variabile "A" di tipo str
+a = 4
+A = "Sally"
+print("#############")
+#attribuiusco ad ogni variabile un nome che non inizi con un numero e non contenga spazi
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+print("#############")
+#nomino le 3 variabili: la prima con due lettere maiuscole, la seconda con 3 lettere maiuscole, la terza con il trattino basso
+myVariableName = "John"
+MyVariableName = "John"
+my_variable_name = "John"
+
+
+
+
