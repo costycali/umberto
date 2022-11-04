734e6b1b0c295f1861ca201d985358ed08e03a0f
costycali <caligiore.costanza@iisgalvanimi.edu.it>
2022-11-04 10:40:02 +0000

    versione1

---
+++ esercizio2.py
@@ -0,0 +1,50 @@
+mylist = ["apple", "banana", "cherry"]
+#definisco una variabile che contiene diversi valori
+
+print("#############")
+#creo una variabile cher contiene diversi valori str e la stampo
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+print("#############")
+#creo una variabile che contiene diversi valori str anche ripetuti, grazie all'operatore thislist
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+print("#############")
+#con l'operatore len posso contare quanti valori contiene la variabile
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+print("#############")
+#definisco 3 variabili che contengono un tipo di valori diversi: la prima variabile contiene valori str, la seconda valori int e la terza valori boolean
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print("#############")
+#definisco una variabile che contiene valori str, int e boolean
+list1 = ["abc", 34, True, 40, "male"]
+
+print("#############")
+#con l'operatore type stampo il tipo di variabile
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+print("#############")
+#uso l'operatore list per creare una lista 
+thislist = list(("apple", "banana", "cherry")) 
+print(thislist)
+
+print("#############")
+#con questo print stampo solo il secondo valore della lista
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+print("#############")
+#stampo l'ultimo valore str della lista
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+print("#############")
+
